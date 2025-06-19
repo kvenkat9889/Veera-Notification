f3cda1faaf270dee94a650247caf060548772503
kvenkat9889 <kvenkat9889@gmail.com>
2025-06-19 13:15:00 +0530

    updated new ip

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3013;
+const port = 3300;
 
 // Middleware
 app.use(cors());
@@ -92,7 +92,7 @@ app.post('/api/notifications', async (req, res) => {
 waitForDatabase().then(() => {
   createTable().then(() => {
     app.listen(port, () => {
-      console.log(`Server running at http://98.80.67.100:${port}`);
+      console.log(`Server running at http://3.85.61.23:${port}`);
     });
   });
 });
